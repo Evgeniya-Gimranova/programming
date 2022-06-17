4091060830fffa16bf55f769dbec5efd8263b650
evgeniya <evgeniya2689@yandex.ru>
2022-06-17 13:26:40 +0500

    add task0

---
+++ Task0/Program.cs
@@ -0,0 +1,6 @@
+﻿Console.Write("Введите целое число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int square = num * num;
+Console.WriteLine(square); 
+Console.Write($"Квадрат числа {num} = {square}");
+
