223723ddb503b5867510121f6a9152864e9eafd6
evgeniya <evgeniya2689@yandex.ru>
2022-06-24 13:18:41 +0500

    Add Task9

---
+++ Task9/Program.cs
@@ -0,0 +1,24 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+int rndNum = new Random().Next(10, 100);
+Console.WriteLine($"Случайное числоиз отрезка 10-99 => {rndNum}");
+ int firstDigit = rndNum / 10;
+ int secondDigit = rndNum % 10;
+// Console.Write($"Наибольшая цифра числа {rndNum} => ");
+
+// if (firstDigit > secondDigit) Console.WriteLine(firstDigit);
+// else Console.WriteLine(secondDigit);
+
+Console.WriteLine(firstDigit > secondDigit ? firstDigit : secondDigit);
+
+int MaxDigit(int num)
+{
+    int firstDigit = num / 10;
+    int secondDigit = num % 10;
+    if (firstDigit == secondDigit) return 0;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+int maxDigit = MaxDigit (rndNum);
+string result = maxDigit > 0 ? maxDigit.ToString() : "цифры одинаковые";
+Console.WriteLine($"Наибольшая цифра числа {rndNum} => {result}");
