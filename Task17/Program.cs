786faadd2ac44c015e9f1774446e88e7f7037b2f
evgeniya <evgeniya2689@yandex.ru>
2022-06-28 12:32:11 +0500

    Add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,35 @@
+﻿Console.WriteLine("Введите координаты точки X и Y");
+Console.Write("X ");
+int x = Convert.ToInt32(Console.ReadLine());
+Console.Write("Y ");
+int y = Convert.ToInt32(Console.ReadLine());
+
+// if (x > 0 && y > 0)
+// {
+//     Console.WriteLine("Первая четверть");
+// }
+// else if (x < 0 && y > 0)
+// {
+//     Console.WriteLine("Вторая четверть");
+// }
+// else if (x < 0 && y < 0)
+// {
+//     Console.WriteLine("Третья четверть");
+// }
+// else if (x > 0 && y < 0)
+// {
+//     Console.WriteLine("Четвертая четверть");
+// }
+// else Console.WriteLine("Введены некорректные данные");
+
+
+string Quard(int xc, int yc)
+{
+    if (xc> 0 && yc > 0) return "Первая четверть";
+    if (xc< 0 && yc > 0) return "Вторая четверть";
+    if (xc< 0 && yc < 0) return "Третья четверть";
+    if (xc> 0 && yc < 0) return "Четвертая четверть";
+    return "Введены некорректные данные";
+}
+string result = Quard(x, y);
+Console.WriteLine(result);
