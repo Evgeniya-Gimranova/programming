4dd293932ff933c1b6a324b49a239b78ed30ab99
evgeniya <evgeniya2689@yandex.ru>
2022-07-01 13:11:04 +0500

    Add Task24

---
+++ Task24/Program.cs
@@ -0,0 +1,21 @@
+﻿Console.WriteLine("Введите число ");
+int number = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+// for (int i = 1; i <= number; i++)
+// {
+//     sum += i;
+// }
+// Console.WriteLine($"{sum}");
+
+
+int Sum(int num)
+{
+    int sum1 = 0;
+    for (int i = 1; i <= num; i++)
+    {
+        sum1 += i;
+    }
+    return sum1;
+}
+sum = Sum(number);
+Console.WriteLine($"Сумма чисел от 1 до {number} = {sum}");
