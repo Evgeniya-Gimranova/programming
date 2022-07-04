7a7aa36d628e2a5cb02d22527c00b1ed242174da
evgeniya <evgeniya2689@yandex.ru>
2022-07-04 09:30:04 +0500

    Add Task30

---
+++ Task30/Program.cs
@@ -0,0 +1,20 @@
+﻿
+
+int[] array= new int[8];
+// int[] array
+
+int index = 0;
+var rnd = new Random();
+while (index <array.Length)
+{
+    array[index]= rnd.Next(0,2);  
+    index ++;
+}
+
+for (int i = 0; i < array.Length; i++)
+{
+    if (i == 0) Console.Write("[");
+    if (i<array.Length - 1) Console.Write(array [i] + " , ");
+    else Console.Write(array[i] + " ] ");
+
+}
