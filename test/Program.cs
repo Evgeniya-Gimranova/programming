6a5ac74ebaec7c70a4b4e6471fd2a124850e59d3
evgeniya <evgeniya2689@yandex.ru>
2022-07-15 16:26:11 +0500

    random double array

---
+++ test/Program.cs
@@ -0,0 +1,29 @@
+﻿//Заполнение массива случайными вещественными числами
+
+
+
+double[] CreateArrayRndDouble(int size, int min, int max)
+{
+    double[] array = new double[size];
+    var rnd = new Random();
+    int index = 0;
+
+    while (index < array.Length)
+    {
+        array[index] = Math.Round((rnd.NextDouble() * (max - min) + min), 2);
+        index++;
+    }
+    return array;
+}
+void PrintArray(double[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i == 0) Console.Write("[");
+        if (i < arr.Length - 1) Console.Write(arr[i] + " , ");
+        else Console.Write(arr[i] + " ] ");
+
+    }
+}
+double[] array = CreateArrayRndDouble(6, 100, 1000);
+PrintArray(array);
