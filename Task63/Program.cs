1a985855fcd2bcd2f90d085bdef532452155b01d
evgeniya <evgeniya2689@yandex.ru>
2022-07-19 13:03:16 +0500

    Add task 63

---
+++ Task63/Program.cs
@@ -0,0 +1,13 @@
+﻿//вывести числа от 1 до N
+
+Console.Clear();
+Console.WriteLine("Введите число ");
+int num = Convert.ToInt32(Console.ReadLine());
+NaturalNumber(num);
+
+void NaturalNumber (int num)
+{
+    if (num==0) return;
+    NaturalNumber (num-1);
+    Console.Write($"{num} ");
+}
